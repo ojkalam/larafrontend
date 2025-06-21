9b3f84c42ae3631c2329d5b6cce29027079368cb
Md Abul Kalam <tipu4142@gmail.com>
2025-06-21 20:24:16 +0600

    fix: dynamic component loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @routes
     @viteReactRefresh
-    @vite( ['resources/css/app.css', 'resources/js/app.js'])
+    @vite( entrypoints: ['resources/css/app.css', 'resources/js/app.js'])
+    {{-- dynamic component loading : "resources/js/Pages/{$page['component']}.vue --}}
     @inertiaHead
 </head>
 
